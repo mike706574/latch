5874ad7e860f26fb23e4e38e9e5d6051fb3d3bf9
EAS7510 <michaeleaster@northwesternmutual.com>
2017-09-23 18:12:58 -0500

    Use group.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mike706574/latch "0.2.1-SNAPSHOT"
+(defproject fun.mike/latch "0.2.1-SNAPSHOT"
   :description "A project."
   :url "https://github.com/mike706574/latch"
   :license {:name "Eclipse Public License"
