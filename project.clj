9196f4535e63cbd2b1dd28ede0a9297a346d8f36
EAS7510 <michaeleaster@northwesternmutual.com>
2017-09-25 10:47:36 -0500

    Release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fun.mike/latch "0.2.3-SNAPSHOT"
+(defproject fun.mike/latch "0.2.3"
   :description "A project."
   :url "https://github.com/mike706574/latch"
   :license {:name "Eclipse Public License"
