90f8385d08e27b3e24f7609160ea72c034f83d46
EAS7510 <michaeleaster@northwesternmutual.com>
2018-04-05 10:00:13 -0500

    Docstrings.

--- src/latch/core.clj
+++ src/latch/core.clj
@@ -4,21 +4,45 @@
             [clojure.string :as str]
             [latch.crypto :as crypto]))
 
-(defn encrypt [text key] (crypto/encrypt text key))
-(defn decrypt [text key] (crypto/decrypt text key))
+(defn encrypt
+  "Encrypts text using key."
+  [text key]
+  (crypto/encrypt text key))
 
-(defn encrypted [path]
+(defn decrypt
+  "Decrypts text using key."
+  [text key]
+  (crypto/decrypt text key))
+
+(defn encrypted
+  "Retrieves the encrypted content from path on the classpath."
+  [path]
   (if-let [resource (io/resource path)]
      (-> resource
          (slurp)
          (str/trim-newline))
      (throw (ex-info (str path " not found.") {:path path}))))
 
-(defn decrypted [path key]
+(defn decrypted
+  "Retrieves and decrypts the content from path on the classpath."
+  [path key]
   (decrypt (encrypted path) key))
 
-(defn encrypted-password [] (encrypted "password.txt"))
-(defn encrypted-token [] (encrypted "token.txt"))
+(defn encrypted-password
+  "Retrieves the encrypted password from password.txt on the classpath."
+  []
+  (encrypted "password.txt"))
 
-(defn decrypted-password [key] (decrypt (encrypted-password) key))
-(defn decrypted-token [key] (decrypt (encrypted-token) key))
+(defn encrypted-token
+  "Retrieves the encrypted token from token.txt on the classpath."
+  []
+  (encrypted "token.txt"))
+
+(defn decrypted-password
+  "Decrypts the password from password.txt on the classpath using key."
+  [key]
+  (decrypt (encrypted-password) key))
+
+(defn decrypted-token
+  "Decrypts the token from token.txt on the classpath using key."
+  [key] (decrypt (encrypted-token) key))
